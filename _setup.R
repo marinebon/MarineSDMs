f13662dd3db3fa99f1a9cb2102a69134adf1a317
Ben Best <ben@ecoquants.com>
2023-11-20 13:52:22 -0500

    _setup.R: install.package(glossary)

--- _setup.R
+++ _setup.R
@@ -1,4 +1,6 @@
 # glossary setup - persistent across chapters
+if (!"glossary" %in% rownames(installed.packages()))
+  install.packages("glossary", repos = "https://debruine.r-universe.dev")
 library(glossary)
 glossary_path("glossary.yml")
 glossary_persistent(TRUE)
